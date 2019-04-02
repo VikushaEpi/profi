275f13eb8b3b43c3e2cc59eadef1bd6cd5d22ae5
VikushaEpi <47805094+VikushaEpi@users.noreply.github.com>
2019-04-02 20:02:56 +0300

    Update HelloWorld.java

--- Raydonme1/src/HelloWorld.java
+++ Raydonme1/src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Akhmadullin A,Epifanova V, Sagitova A ,Maksimova A,Orlov D");
+		System.out.println("Akhmadullin A,Epifanova Viktoriya, Sagitova A ,Maksimova A,Orlov D");
 	}
 
 }
